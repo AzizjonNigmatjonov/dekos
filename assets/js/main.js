3ceb682d0245a3a498e04f326b1e43d5f357143e
Muhkammadaziz <aziz.nigmatjonov7@gmail.com>
2022-01-05 17:48:36 +0500

    aDDED

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,36 @@
-window.addEventListener('DOMContentLoaded', (e) => {
-    e.preventDefault();
-    
-    const clientsCounter = document.getElementById('clientCount');
-    const servicesCounter = document.getElementById('serviceCount');
-    const doctorsCounter = document.getElementById('doctorCount');
-    
-    let onlyOnce = true;
-    
-    window.onscroll = function() {
-        start();
-    }
-    
-    function start() {
-        if (window.scrollY > 0 && onlyOnce) {
-            onlyOnce = false;
-            if (clientsCounter && servicesCounter && doctorsCounter) {
-                animateValue(clientsCounter, 0, clientsCounter.innerText, 3000);
-                animateValue(servicesCounter, 0, servicesCounter.innerText, 3000);
-                animateValue(doctorsCounter, 0, doctorsCounter.innerText, 3000);
-            }
+const clientsCounter = document.getElementById('clientCount');
+const servicesCounter = document.getElementById('serviceCount');
+const doctorsCounter = document.getElementById('doctorCount');
+let onlyOnce = true;
+start();
+function start() {
+    if (onlyOnce) {
+        onlyOnce = false;
+        if (clientsCounter && servicesCounter && doctorsCounter) {
+            animateValue(clientsCounter, 0, clientsCounter.innerText, 5000);
+            animateValue(servicesCounter, 0, servicesCounter.innerText, 5000);
+            animateValue(doctorsCounter, 0, doctorsCounter.innerText, 5000);
         }
     }
+}
+function animateValue(obj, start, end, duration) {
     
-    function animateValue(obj, start, end, duration) {
-        
-        if (start === end) {
-            return;
-        }
-        
-        const range = end - start;
-        let current = start;
-        const increment = end > start ? 1 : -1;
-        const stepTime = Math.abs(Math.floor(duration / range));
-        const timer = setInterval(() => {
-            current += increment;
-            obj.innerHTML = current;
-            if (current == end) {
-                clearInterval(timer);
-            }
-        }, stepTime);
+    if (start === end) {
+        return;
     }
     
-});
+    const range = end - start;
+    let current = start;
+    const increment = end > start ? 1 : -1;
+    const stepTime = Math.abs(Math.floor(duration / range));
+    const timer = setInterval(() => {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
 
 window.onload = function() {
     const el = document.getElementById('overlay');
@@ -63,10 +52,10 @@ if (categoryPage) {
     const categorySwitcherSelectorWrapper = document.querySelector('.category-switcher-selector-wrapper');
     const categorySwitcherModal = document.querySelector('.category-switcher-modal');
     categorySwitcherTexts.forEach(function(el) {
-       el.addEventListener('click', element => {
-           categorySwitcherSelector.textContent = element.target.innerHTML;
-       });
+        el.addEventListener('click', element => {
+            categorySwitcherSelector.textContent = element.target.innerHTML;
+        });
     });
     
-  
+    
 }
